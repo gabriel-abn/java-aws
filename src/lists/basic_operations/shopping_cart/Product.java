4d63115e20ad2c0ea487b8c5c102a6be466cb791
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-08 10:32:24 -0300

    feat: :sparkles: concluded basic operations with ArrayLists

---
+++ src/lists/basic_operations/shopping_cart/Product.java
@@ -0,0 +1,29 @@
+package lists.basic_operations.shopping_cart;
+
+public class Product {
+  private String name;
+  private double price;
+  private int quantity;
+
+  public Product(String nameAtStart, double priceAtStart, int quantityAtStart) {
+    this.name = nameAtStart;
+    this.price = priceAtStart;
+    this.quantity = quantityAtStart;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public int getQuantity() {
+    return this.quantity;
+  }
+
+  public void printProduct() {
+    System.out.println(this.name + ", price " + this.price + ", quantity " + this.quantity);
+  }
+}
---
+++ src/lists/basic_operations/tasks_list/Task.java
@@ -0,0 +1,13 @@
+package lists.basic_operations.tasks_list;
+
+public class Task {
+  String description;
+
+  public Task(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+}
