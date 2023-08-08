a80d0861188d63fbfcd0fb7e4b73e20b1d9d976c
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-08 19:02:42 -0300

    feat: :sparkles: finished HashSet and TreeSet exercices

---
+++ src/Sets/SortingOperations/StudentList/Student.java
@@ -0,0 +1,61 @@
+package Sets.SortingOperations.StudentList;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Student {
+  private String name;
+  private int grade;
+  private Long enrollment;
+
+  public Student(String name, int age, long enrollment) {
+    this.name = name;
+    this.grade = age;
+    this.enrollment = enrollment;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getGrade() {
+    return this.grade;
+  }
+
+  public Long getEnrollment() {
+    return this.enrollment;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+
+    if (!(obj instanceof Student student)) {
+      return false;
+    }
+
+    return this.enrollment.equals(student.getEnrollment());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.enrollment);
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+        "name='" + name + '\'' +
+        ", age=" + grade +
+        '}';
+  }
+}
+
+class CompareStudentsByGrade implements Comparator<Student> {
+  @Override
+  public int compare(Student student1, Student student2) {
+    return Integer.compare(student1.getGrade(), student2.getGrade());
+  }
+}
