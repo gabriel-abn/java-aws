d6b01a4e5b876fa0b7a0511e872fe8572dc743e4
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-09 17:48:31 -0300

    feat: :sparkles: implemented consumer interface exemples

---
+++ src/streams/functinalInterfaces/ConsumerExemple.java
@@ -0,0 +1,39 @@
+package streams.functinalInterfaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExemple {
+  public static void main(String[] args) {
+    List<String> names = Arrays.asList("John Cage", "Freddy Krueger",
+        "Samuel Francis",
+        "John Snow", "Samuel Jackson");
+
+    // Usando a interface Consumer
+    Consumer<String> printSamuel = name -> {
+      if (name.contains("Samuel")) {
+        System.out.println("Hello " + name);
+      }
+    };
+
+    names.stream().forEach(printSamuel);
+
+    // Usando uma função lambda
+    names.forEach(name -> {
+      if (name.contains("John")) {
+        System.out.println("Greatings " + name);
+      }
+    });
+
+    // Usando o método accept
+    names.stream().forEach(new Consumer<String>() {
+      @Override
+      public void accept(String name) {
+        if (name.contains("Freddy")) {
+          System.out.println("Welcome " + name);
+        }
+      }
+    });
+  }
+}
