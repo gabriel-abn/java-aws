5419c3a4dd90ca11c987f3039305677e113a657a
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-09 17:58:42 -0300

    feat: :sparkles: implemented Supplier Interface exemple

---
+++ src/streams/functinalInterfaces/SupplierExemple.java
@@ -0,0 +1,19 @@
+package streams.functinalInterfaces;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExemple {
+  public static void main(String[] args) {
+    // Usando uma função lambda, que retorna um inteiro aleatório. O método
+    // implementado é o "get".
+    Supplier<Integer> supplier = () -> (int) (Math.random() * 1000);
+
+    // Usando a interface Supplier
+    List<Integer> list = Stream.generate(supplier).limit(5).toList();
+
+    // Usando referência de método
+    list.forEach(System.out::println);
+  }
+}
