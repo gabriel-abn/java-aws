70310abe1896e68ee57aa0a1920c187d26670305
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-08 23:07:19 -0300

    feat: :sparkles: concluded find operations with HashMaps

---
+++ src/Map/FindOperations/ProductsStorage/ProductStorage.java
@@ -0,0 +1,94 @@
+package Map.FindOperations.ProductsStorage;
+
+import java.util.HashMap;
+
+public class ProductStorage {
+  private HashMap<Long, Products> products;
+
+  public ProductStorage() {
+    this.products = new HashMap<Long, Products>();
+  }
+
+  public void addProduct(Long id, String name, double price, int quantity) {
+    this.products.put(id, new Products(name, price, quantity));
+  }
+
+  public void showProducts() {
+    if (this.products.isEmpty()) {
+      throw new RuntimeException("The products list is empty");
+    }
+
+    System.out.println("Products: ");
+    for (Long id : this.products.keySet()) {
+      System.out.println(id + ": " + this.products.get(id));
+    }
+  }
+
+  public double getTotalProductsPrice() {
+    if (this.products.isEmpty()) {
+      throw new RuntimeException("The products list is empty");
+    }
+
+    double total = 0;
+    for (Products product : this.products.values()) {
+      total += product.getPrice();
+    }
+
+    return total;
+  }
+
+  public void getHigherPriceProduct() {
+    if (this.products.isEmpty()) {
+      throw new RuntimeException("The products list is empty");
+    }
+
+    Products higherPriceProduct = null;
+    double higherPrice = Double.MIN_VALUE;
+
+    for (Products product : this.products.values()) {
+      if (product.getPrice() > higherPrice) {
+        higherPrice = product.getPrice();
+        higherPriceProduct = product;
+      }
+    }
+
+    System.out.println("Higher price product: " + higherPriceProduct);
+  }
+
+  public void getLowerPriceProduct() {
+    if (this.products.isEmpty()) {
+      throw new RuntimeException("The products list is empty");
+    }
+
+    Products lowerPriceProduct = null;
+    double lowerPrice = Double.MAX_VALUE;
+
+    for (Products product : this.products.values()) {
+      if (product.getPrice() < lowerPrice) {
+        lowerPrice = product.getPrice();
+        lowerPriceProduct = product;
+      }
+    }
+
+    System.out.println("Lower price product: " + lowerPriceProduct);
+  }
+
+  public void getHigherQuantityProductWithTotalPrice() {
+    if (this.products.isEmpty()) {
+      throw new RuntimeException("The products list is empty");
+    }
+
+    Products higherQuantityProduct = null;
+    int higherQuantity = Integer.MIN_VALUE;
+
+    for (Products product : this.products.values()) {
+      if (product.getQuantity() > higherQuantity) {
+        higherQuantity = product.getQuantity();
+        higherQuantityProduct = product;
+      }
+    }
+
+    System.out.println("Higher quantity product: " + higherQuantityProduct);
+    System.out.println("Total price: " + higherQuantityProduct.getPrice() * higherQuantityProduct.getQuantity());
+  }
+}
---
+++ src/Map/FindOperations/ProductsStorage/Products.java
@@ -0,0 +1,29 @@
+package Map.FindOperations.ProductsStorage;
+
+public class Products {
+  private String name;
+  private double price;
+  private int quantity;
+
+  public Products(String name, double price, int quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public int getQuantity() {
+    return this.quantity;
+  }
+
+  public String toString() {
+    return this.name + ": " + this.price + "€, " + this.quantity + " units";
+  }
+}
